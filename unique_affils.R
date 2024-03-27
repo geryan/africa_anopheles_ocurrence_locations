abe8e83afe5f7d4208dee9108a20c9c0b08f48c5
geryan <geryan@users.noreply.github.com>
2024-03-27 12:33:38 +1100

    document it

--- unique_affils.R
+++ unique_affils.R
@@ -4,7 +4,12 @@ library(lubridate)
 
 affils <- read_csv("data/Affiliation spreadsheet (version 2).csv")
 
-
+# select only the affiliation columns
+# get rid of any yet to be filled in (NA)
+# get unique combinations of affiliation_simple and affiliation
+# order by affiliation_simple
+# NB repeats of affiliation_simple are expected as long affiliation might
+# be found in different formats, e.g. different departments etc.
 unique_affils <- affils |>
   select(affiliation_simple, affiliation) |>
   filter(!is.na(affiliation)) |>
@@ -13,6 +18,7 @@ unique_affils <- affils |>
 
 unique_affils
 
+# write to file with date-stamp
 write_csv(
   x = unique_affils,
   file = sprintf(
